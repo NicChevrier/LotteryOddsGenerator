b1005d6e301e855938db8de8d8dc20b67f58417a
NicChevrier <94065816+NicChevrier@users.noreply.github.com>
2023-01-05 10:48:45 -0400

    Update Odds.py

--- Odds.py
+++ Odds.py
@@ -1,1 +1,11 @@
+import random
 
+# Generate 6 random unique numbers and a bonus number
+numbers = random.sample(range(1, 50), 6)
+bonus = random.randint(1, 49)
+
+# Print the numbers with a message
+print("Here are this week's lucky numbers:")
+for i, num in enumerate(numbers):
+    print(num, end=' ')
+print("(Bonus: {})".format(bonus))
